3c08648bc448588f193ff40ec6192b9e665a689a
Nanda Kishore <yvvnandak@gmail.com>
2025-10-15 22:11:11 +0530

    fourth commit

--- src/main/java/com/javaConnect/web/config/EnvLoader.java
+++ src/main/java/com/javaConnect/web/config/EnvLoader.java
@@ -1,19 +1,26 @@
 package com.javaConnect.web.config;
 
-
 import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.annotation.PostConstruct;
-
 import org.springframework.context.annotation.Configuration;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Configuration
 public class EnvLoader {
+
     @PostConstruct
     public void loadEnv() {
-        Dotenv dotenv = Dotenv.configure()
-                               .filename("db.env")  // specify your custom file
-                               .load();
-        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+        Path envPath = Path.of("db.env"); // check if file exists
+        if (Files.exists(envPath)) {
+            Dotenv dotenv = Dotenv.configure()
+                                  .filename("db.env")
+                                  .load();
+            dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+            System.out.println("Loaded db.env locally");
+        } else {
+            System.out.println("db.env not found, skipping EnvLoader (Render environment)");
+        }
     }
 }
