4a9066f5d803898a6f42bb0a0e66bd182199c6c8
Nanda Kishore <yvvnandak@gmail.com>
2025-10-24 18:52:27 +0530

    added email sender2

--- src/main/java/com/javaConnect/web/serviceImpl/EmailServiceImpl.java
+++ src/main/java/com/javaConnect/web/serviceImpl/EmailServiceImpl.java
@@ -1,9 +1,11 @@
 package com.javaConnect.web.serviceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
 
 import com.javaConnect.web.entity.User;
 import com.javaConnect.web.service.EmailService;
